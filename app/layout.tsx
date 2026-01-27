31270124409f67087d25795061d0ae8058b802b3
jaredazcarate <jaredazcarate@gmail.com>
2026-01-27 00:07:55 +0000

    fix: build errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Agendamentos do Templo",
-  description: "Sistema de gestão de caravanas ao templo",
+  description: "Sistema de gestão de caravanas ao templo da Estaca Porto Norte",
 };
 
 export default function RootLayout({
